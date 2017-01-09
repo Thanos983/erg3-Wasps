16a126268f598a6c555fdb75ef4a89d394ed58a5
Thanos983 <Thanos983@hotmail.gr>
2017-01-09 18:43:34 +0200

    fixed fitness function. Bug on class Solutions remains

--- wasps.py
+++ wasps.py
@@ -16,30 +16,30 @@ class Bomb:
     def getX(self):
         return self.x1, self.y1
 
-    def fitness(self, dmax, nests, number_of_nests):
+    def fitness(self, cnests):
         """
 
         :param dmax: Maximum Distance
-        :param nests: Coordinates of nests
-        :param number_of_nests: Population of nests
+        :param cnests: Coordinates of nests
+        :param number_of_nests: NumberOfSolutions of nests
         :return: How many kills did this bomb to all nests
         """
-
+        dmax = maxDistance(cnests)
         kills = 0
         distance = 0
-        for i in range(1, number_of_nests+1):
-            distance += ((nests[i][0] - self.x1) ** 2 + (nests[i][1] - self.y1) ** 2)
-            damage = int(nests[i][2] * (dmax / (20 * distance + 0.00001)))  # Many zeros occur due to truncation
+        damage = 0
 
-            if damage > nests[i][2]:
-                nests[i][2] = 0
+        for i in range(1, cnests.__len__()+1):
+            distance = math.sqrt((cnests[i][0] - self.x1) ** 2 + (cnests[i][1] - self.y1) ** 2)
+            damage = round(cnests[i][2] * dmax / (20 * distance + 0.00001))  # Many zeros occur due to truncation
+
+            if damage > cnests[i][2]:
+                cnests[i][2] = 0
             else:
-                nests[i][2] -= damage
+                cnests[i][2] -= damage
+                self.count += damage
 
-            kills += damage
-
-        print(int(kills))
-        return int(kills)
+        return self.count
 
 
 class Solution:
@@ -48,34 +48,33 @@ class Solution:
     """
     bombPos = []
 
-    def __init__(self, x1, y1, x2, y2, x3, y3):
-        Solution.bombPos.append(Bomb(x1, y1))
-        Solution.bombPos.append(Bomb(x2, y2))
-        Solution.bombPos.append(Bomb(x3, y3))
-        self.fitness = 0
+    def __init__(self, x1, y1, x2, y2, x3, y3, cnests):
+        self.bombPos.append(Bomb(x1, y1))
+        self.bombPos.append(Bomb(x2, y2))
+        self.bombPos.append(Bomb(x3, y3))
+        self.fitness = self.SolutionFitness(cnests)
 
     def get_Pos(self):
         return self.bombPos
 
-    def SolutionFitness(self, nests, numberOfNests):
+    def SolutionFitness(self, cnests):
         """
 
         :param dmax: Maximum distance of nests
-        :param nests: dictionary containing coordinates of nest
+        :param cnests: dictionary containing coordinates of nest
         :param numberOfNests: how many nests exist
         :return: How many death of wasps occur by this solution
         """
-        dmax = maxDistance(nests, numberOfNests)
 
         count = 0
 
         for i in range(3):  # Number of bombs
-            count += self.bombPos[i].fitness(dmax, nests, numberOfNests)
+            count += self.bombPos[i].fitness(cnests)
 
-        self.fitness = count
+        return count
 
 
-def maxDistance(nest, numberOfNests):
+def maxDistance(nest):
     """
     Finds the maximum distance(dmax) between nests
     Input: a dictionary with nests coordinates and the number of nests
@@ -83,8 +82,8 @@ def maxDistance(nest, numberOfNests):
     """
     dmax = -1
 
-    for p in range(1, numberOfNests + 1):
-        for k in range(p + 1, numberOfNests + 1):
+    for p in range(1, nest.__len__() + 1):
+        for k in range(p + 1, nest.__len__() + 1):
             distance = math.sqrt((nest[p][0] - nest[k][0]) ** 2 + (nest[p][1] - nest[k][1]) ** 2)
 
             if distance > dmax:
@@ -93,53 +92,67 @@ def maxDistance(nest, numberOfNests):
     return int(dmax * 1000) / 1000  # Truncates dmax to 3 digits precision
 
 
-def main():
-    NubmerOfBombs = 3
+def openFile():
+    """
+    Opens the file and returns a dictionary with the coordinates of nest and the number of bees inside
+    """
 
-    # reading nests co-ordinates from a txt
-    nests = {}
+    anests = {}
+    count = 0
     count = 0
 
     with open("nests.txt", "r") as file:
         for line in file:
             reader = line.split()
             count += 1
-            nests[count] = [int(reader[0]), int(reader[1]), int(reader[2])]
+            anests[count] = [int(reader[0]), int(reader[1]), int(reader[2])]
 
+    return anests
+
+
+def visualiseNests(nests):
+
+    # Separate x,y values in order to create a graph of nests
     x_axis = []
     y_axis = []
-    numberOfWasps = []
+    number_of_wasps = []
 
     # pass nests coordinates in separate lists
-    for c in range(1, count + 1):
+    for c in range(1, nests.__len__):
         x_axis.append(nests[c][0])
         y_axis.append(nests[c][1])
-        numberOfWasps.append(nests[c][2])
+        number_of_wasps.append(nests[c][2])
 
     # Visualize nests
     # mp.plot(x_axis, y_axis, 'mD')
     # mp.axis([0, 100, 0, 100])
     # mp.show()
 
-    # Populate bombs with random population. Will have 6 float numbers with 3 digit precision
+
+def main():
+    nubmerofbombs = 3
+
+    # reading nests co-ordinates from a txt
+    nests = openFile()
 
     bombs = []
-    population = 5  # Preferable population 100000
+    NumberOfSolutions = 3  # Preferable NumberOfSolutions 100000
 
-    for i in range(population):  # Preferable range 100000
+    # Populate bombs with random NumberOfSolutions. Will have 6 float numbers with 3 digit precision
+    for i in range(NumberOfSolutions):  # Preferable range 100000
         randomBomb = random.sample(range(100000), 6)
         randomBomb[:] = [x / 1000 for x in randomBomb]
-
-        bombs.append(Solution(randomBomb[0], randomBomb[1], randomBomb[2], randomBomb[3], randomBomb[4], randomBomb[5]))
+        copy_nests = nests.copy()
+        bombs.append(Solution(randomBomb[0], randomBomb[1], randomBomb[2], randomBomb[3], randomBomb[4], randomBomb[5], copy_nests))
+        print(nests)
+        print(copy_nests)
+        print()
 
     testVar = bombs[0].get_Pos()[0].getX()[1]
     # Finally got it. Accessed into a variable of class bomb
 
-    test = bombs[0].SolutionFitness(nests, count)
-
-    dmax = maxDistance(nests, count)
-
-    print(nests)
+    for i in range(NumberOfSolutions):
+        print(bombs[i].fitness)
 
 if __name__ == '__main__':
     main()
