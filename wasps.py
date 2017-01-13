222dd94324e383d129fce7791846e5d83afd58f7
Thanos983 <Thanos983@hotmail.gr>
2017-01-13 16:29:18 +0200

    Added probability function and tested

--- wasps.py
+++ wasps.py
@@ -61,6 +61,7 @@ class Solution:
         self.b2 = Bomb(x2, y2)
         self.b3 = Bomb(x3, y3)
         self.fitness = self.SolutionFitness(cnests)
+        self.probability = 0
 
     def SolutionFitness(self, cnests):
         """
@@ -68,6 +69,24 @@ class Solution:
         :return: The total fitness of solution
         """
         return self.b1.fitness(cnests) + self.b2.fitness(cnests) + self.b3.fitness(cnests)
+
+    def calculate_probability_of_solution(self, lenght, position_of_solution):
+        """
+        Calculates the probability of the soltion based on the Rank Roulete Wheel
+        and stores it in a variable (p = (lenght + position_of_solution + 1) / sum_of_solutions)
+        input : lenght--> lenght of the list of Bombs
+                position_of_solution --> the position i in the list
+        Output: returns the probability of the Solution
+        """
+
+        sum_of_solutions = 0
+        for i in range(1,lenght+1):
+            sum_of_solutions += i
+
+        print(sum_of_solutions)
+
+        self.probability = (lenght - position_of_solution + 1)/ sum_of_solutions
+
 
 
 # <================================== End of class Solution ==============================>
@@ -126,13 +145,15 @@ def visualiseNests(nests):
         # mp.axis([0, 100, 0, 100])
         # mp.show()
 
-
-def main():
+def create_random_population(number_of_solutions):
+    """
+    Creates N random solutions and returns them in a sorted list
+    input: N = number_of_solutions
+    Output: Bombs = a list of solution
+    """
     bombs = []
-    number_of_solutions = 10000  # Preferable number_of_solutions 100000
-
-    # Populate bombs with random number_of_solutions. Will have 6 float numbers with 3 digit precision
-    for i in range(number_of_solutions):  # Preferable range 100000
+    #  Populate bombs with random number_of_solutions. Will have 6 float numbers with 3 digit precision
+    for i in range(number_of_solutions):  # Preferable range 1000
         random_bomb = random.sample(range(100000), 6)
         random_bomb[:] = [x / 1000 for x in random_bomb]
         copy_nests = openFile()  # reading the coordinates of the nests. That should be done once before for loop
@@ -140,10 +161,22 @@ def main():
                               random_bomb[5], copy_nests))
 
     bombs.sort(key=key_fun, reverse=True)
+    return bombs
 
-    for i in bombs:
-        print(i.fitness)
 
+def main():
+
+    number_of_solutions = 6  # Preferable number_of_solutions 1000
+    Bombs = create_random_population(number_of_solutions) #  Populate Bombs with random solution
+
+    s = 0
+
+    for i in range(0, number_of_solutions):
+        Bombs[i].calculate_probability_of_solution(number_of_solutions, i+1)
+        s+= Bombs[i].probability
+        print(i, Bombs[i].fitness, Bombs[i].probability*100)
+
+    print(s)
 
 if __name__ == '__main__':
     main()
